d43e879d9fb86649c937f785e92ee64a0db1bec8
Beau Simensen <beau@dflydev.com>
2013-02-14 10:43:45 -0600

    New format controller.

--- src/Depot/Api/Server/Symfony/Controller/Profile/GetProfileInfoController.php
+++ src/Depot/Api/Server/Symfony/Controller/Profile/GetProfileInfoController.php
@@ -2,35 +2,12 @@
 
 namespace Depot\Api\Server\Symfony\Controller\Profile;
 
-use Depot\Api\Server\Symfony\ResponseFactory;
 use Depot\Core\Model;
-use Symfony\Component\HttpFoundation\Request;
 
 class GetProfileInfoController
 {
-    protected $jsonRenderer;
-    protected $entityRepository;
-    protected $responseFactory;
-
-    public function __construct(
-        JsonRendererInterface $jsonRenderer,
-        Model\Entity\EntityRepositoryInterface $entityRepository,
-        ResponseFactory $responseFactory = null
-    )
+    public function action(Model\Entity\EntityInterface $entity, $type)
     {
-        $this->jsonRenderer = $jsonRenderer;
-        $this->entityRepository = $entityRepository;
-        $this->responseFactory = $responseFactory ?: new ResponseFactory;
-    }
-
-    public function action(Request $request, $type)
-    {
-        $entityUri = $request->attributes->get('depot.entity');
-
-        $entity = $this->entityRepository->findByUri($entityUri);
-
-        return $this->responseFactory->createTentJsonResponse(
-            $this->jsonRenderer->render($entity->findProfileInfo($type))
-        );
+        return $entity->findProfileType($type);
     }
 }
